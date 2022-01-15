6ba25d334d52836f147de3f83868a19cd554dd19
anikethpawar1 <97774557+anikethpawar1@users.noreply.github.com>
2022-01-15 09:34:06 +0530

    Add files via upload

---
+++ HCJFV/script.js
@@ -0,0 +1,36 @@
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (name.value === '' || name.value == null) {
+    messages.push('Name is required')
+  }
+
+  if (password.value.length <= 6) {
+    messages.push('Password must be longer than 6 characters')
+  }
+
+  if (password.value.length >= 20) {
+    messages.push('Password must be less than 20 characters')
+  }
+
+  if (password.value === 'password') {
+    messages.push('Password cannot be password')
+  }
+
+  if(name.value != 'abhijeet21') {
+      messages.push('Incorrect user')
+  }
+
+  if(password.value !='abhi@123') {
+      messages.push('Incorrect password')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
